437178857f4d3cf9bb30fb82fc8f9773f299c75b
Gian124a <140373193+Gian124a@users.noreply.github.com>
2023-09-13 15:51:07 -0400

    Add files via upload

---
+++ Funcion.js
@@ -0,0 +1,55 @@
+function Boton(){
+    let Menu = document.getElementById('Menu');
+    let nav = document.getElementById('nav').style.display = 'none';
+
+    if (Menu.style.display === 'none') {
+        Menu.style.display = 'initial';
+    }
+    else{
+        Menu.style.display = 'none';
+    }
+}
+function Boton2(){
+    let Menu = document.getElementById('Menu').style.display = 'none';
+    let nav = document.getElementById('nav');
+    if (nav.style.display === 'none') {
+        nav.style.display = 'initial';
+    }
+    else{
+        nav.style.display = 'none';
+    }
+}
+function Opciones(){
+    let Perfil = document.getElementById('Perfil-Datos');
+    let Editar = document.getElementById('Ajustes');
+    let Configuracion = document.getElementById('Confi');
+
+    Perfil.style.display = 'grid';
+    Editar.style.display = 'none';
+    Configuracion.style.display = 'none';
+}
+function Opciones2(){
+    let Perfil = document.getElementById('Perfil-Datos');
+    let Editar = document.getElementById('Ajustes');
+    let Configuracion = document.getElementById('Confi');
+
+    Perfil.style.display = 'none';
+    Editar.style.display = 'grid';
+    Configuracion.style.display = 'none';
+}
+function Opciones3(){
+    let Perfil = document.getElementById('Perfil-Datos');
+    let Editar = document.getElementById('Ajustes');
+    let Configuracion = document.getElementById('Confi');
+
+    Perfil.style.display = 'none';
+    Editar.style.display = 'none';
+    Configuracion.style.display = 'grid';
+}
+function Botoncito() {
+    const Boton = document.getElementById('Button');
+    const Fondo = document.getElementById('Fondo-Boton');
+    const Boton2 = document.getElementById('Boton');
+    Boton.classList.toggle('Active');
+    Boton2.classList.toggle('Active');
+}
